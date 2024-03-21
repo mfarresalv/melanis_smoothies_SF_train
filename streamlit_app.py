7e03494de2e965e1a73f35275a17028abfc94e5e
mfarresalv <64525692+mfarresalv@users.noreply.github.com>
2024-03-21 09:23:46 +0100

    extract json from request

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, {}!'.format(name_on_order), icon="✅")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
