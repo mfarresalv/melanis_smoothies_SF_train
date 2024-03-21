431d87a0e576bbe08262669e6d5bd62b68d4c5e1
mfarresalv <64525692+mfarresalv@users.noreply.github.com>
2024-03-21 10:34:18 +0100

    table clean

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
